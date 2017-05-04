dc0af34a20f24c8ec20fc1f564391a5b81cdf3cf
Ebonsignori <Ebonsignori>
2017-05-04 18:05:09 -0400

    Created blog layout with bootstrap

--- blog.php
+++ blog.php
@@ -11,26 +11,145 @@
 
         <title> Evan's Blog </title>
         <meta name="author" content="Evan Bonsignori">
-        <meta name="description" content="Currently A Work In Progress.">
+        <meta name="description" content="Currently a work in progress.">
 
         <!-- Open Graph Data -->
         <meta property="og:image" content="img/evan_profile_redrocks_headcrop.jpg">
-        <meta property="og:description" content="Currently A Work In Progress.">
+        <meta property="og:description" content="Currently a work in progress.">
         <meta property="og:title" content="Evan Bonsignori's Blog">
 
         <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="stylesheet" type="text/css" href="css/blog.css">
         <link rel="stylesheet"
               href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    </head>
 
-    <body class="bg-triangle">
-        <div class="content-center-children-center" />
-        <h1> Blog </h1>
-        <h2> <i> A Work In Progress </i> </h2>
-        <p> Will be created with Python's Django framework. </p>
-    </div>
+              <!-- Bootstrap 3.3.7 -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  </head>
 
-    <ul class="icons">
+    <body class="bg-blog">
+
+      <!-- Naviagtion -->
+      <nav class="navbar navbar-default">
+
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html">Evan Bonsignori</a>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <form class="navbar-form navbar-right">
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search">
+              </div>
+              <button type="submit" class="btn btn-default">Find</button>
+            </form>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+
+      </nav>
+
+          <!-- Page Content -->
+          <div class="container">
+            <ul class="nav nav-tabs">
+              <li role="presentation" class="active"><a href="#">Home</a></li>
+              <li role="presentation" class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                  Most Popular <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                  ...
+                </ul>
+              </li>
+              <li role="presentation" class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                  Categories <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                  <li role="presentation"><a href="#">Self Improvement</a></li>
+                  <li role="presentation"><a href="#">Political Comentary</a></li>
+                  <li role="presentation"><a href="#">Comedy</a></li>
+                  <li role="presentation"><a href="#">Tutorials</a></li>
+                </ul>
+              </li>
+
+            </ul>
+          <div class="jumbotron">
+
+                      <!-- First Blog Post -->
+                      <h2>
+                          <a href="#">Blog Post Title</a>
+                      </h2>
+                      <p class="lead">
+                          by <a href="index.php">Start Bootstrap</a>
+                      </p>
+                      <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+                      <hr>
+                      <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                      <hr>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+                      <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                      <hr>
+
+                      <!-- Second Blog Post -->
+                      <h2>
+                          <a href="#">Blog Post Title</a>
+                      </h2>
+                      <p class="lead">
+                          by <a href="index.php">Start Bootstrap</a>
+                      </p>
+                      <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
+                      <hr>
+                      <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                      <hr>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, quasi, fugiat, asperiores harum voluptatum tenetur a possimus nesciunt quod accusamus saepe tempora ipsam distinctio minima dolorum perferendis labore impedit voluptates!</p>
+                      <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                      <hr>
+
+                      <!-- Third Blog Post -->
+                      <h2>
+                          <a href="#">Blog Post Title</a>
+                      </h2>
+                      <p class="lead">
+                          by <a href="index.php">Start Bootstrap</a>
+                      </p>
+                      <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
+                      <hr>
+                      <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                      <hr>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, voluptates, voluptas dolore ipsam cumque quam veniam accusantium laudantium adipisci architecto itaque dicta aperiam maiores provident id incidunt autem. Magni, ratione.</p>
+                      <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                      <hr>
+
+                      <!-- Pager -->
+                      <ul class="pager">
+                          <li class="previous">
+                              <a href="#">&larr; Older</a>
+                          </li>
+                          <li class="next">
+                              <a href="#">Newer &rarr;</a>
+                          </li>
+                      </ul>
+              </div>
+              <!-- /.row -->
+              </div>
+
+              <hr>
+    <div class="container">
+      <footer class="footer">
+    <ul class="icons" >
           <li><a href="https://www.facebook.com/ebonsignori" target="_blank">
                   <span class="fa fa-facebook"></span></a></li>
           <li><a href="https://github.com/Ebonsignori" target="_blank">
@@ -40,12 +159,18 @@
           <li><a href="https://www.instagram.com/ebonsignori/" target="_blank">
                   <span class="fa fa-instagram"></span></a></li>
       </ul>
+      <div class="home">
+           <button class="fa fa-home" onclick="location.href = '../index'">Home</button>
+       </div>
+       <br />
+      </footer>
+      </div>
 
-        <div class="home">
-        <span>
-            <button class="fa fa-home" onclick="location.href = 'index'">Home</button>
-        </span>
-        </div>
+      <!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/main.js">
+
+</script>
 
     </body>
 
