414a7d74fa24632721ad708487c60030a2beec9d
Ebonsignori <Ebonsignori>
2017-05-05 15:48:45 -0400

    Removed margin transition on mobile

--- js/main.js
+++ js/main.js
@@ -2,13 +2,19 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     var rightSide = document.getElementsByClassName("dim")[0];
-    rightSide.style.marginLeft = "265px";
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+    } else {
+      rightSide.style.marginLeft = "265px";
+    }
     rightSide.style.opacity = ".4";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     var rightSide = document.getElementsByClassName("dim")[0];
-    rightSide.style.marginLeft = "0";
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+    } else {
+      rightSide.style.marginLeft = "0";
+    }
     rightSide.style.opacity = "1";
 }
