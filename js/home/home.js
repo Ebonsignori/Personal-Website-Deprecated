3b7c8c1cc383a9803e3c3a61b140d902df79b00e
Evan <evanbonsignori@yahoo.com>
2017-08-09 18:15:13 -0400

    Replace nav menu with dot nav menu.
    TODO: Mobile Nav Menu Overlay

--- js/home/home.js
+++ js/home/home.js
@@ -1,7 +1,3 @@
-$(window).on('load', function () {
-  $('#background-slider').css('background-image', "url('img/homepage_backgrounds/background (2).png')");
- });
-
 var circleText = $('.circle-text span');
 $('.circle-text-seperator').click(function() {
   if (circleText.css('animation-play-state') == 'running') {
